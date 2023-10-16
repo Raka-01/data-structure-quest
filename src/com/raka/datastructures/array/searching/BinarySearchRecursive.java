2ad448f1a12bd82db49424f692e1b13c43407207
Rakesh Kumar <rkbokaro00@gmail.com>
2023-10-16 23:52:20 +0530

    added-array-searching

---
+++ src/com/raka/datastructures/array/searching/BinarySearchRecursive.java
@@ -0,0 +1,54 @@
+package com.raka.datastructures.array.searching;
+
+import java.util.Scanner;
+
+public class BinarySearchRecursive {
+
+    public static int searchElement(int[] array, int lb, int ub, int key){
+        if(lb <= ub){                   // case '=' when lb == ub as set and called from within the method
+            int mid = lb + (ub - lb)/2;
+
+            if(array[mid] == key){
+                return mid;
+            } else if (array[mid] < key) {
+                lb = mid + 1;
+                return searchElement(array, lb, ub, key);       // calling this method on the 2nd half
+            } else if (array[mid] > key) {
+                ub = mid - 1;
+                return searchElement(array, lb, ub, key);       // calling this method on 1st half
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int[] array = {10, 12, 13, 17, 18, 22, 25, 29, 35, 38, 44, 48, 55};
+
+        System.out.println("Original array - ");
+        for(int element : array){
+            System.out.print(element + "   ");
+        }
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter the element to search");
+        int key = input.nextInt();
+
+        int index = searchElement(array, 0, array.length-1, key);
+        if(index > -1){
+            System.out.println("Element found at index " + index);
+        }
+        else {
+            System.out.println("Element Not Found");
+        }
+    }
+}
+
+/**
+ *  Time Complexity: O(log N)
+ *  Auxiliary Space: O(log N)
+ */
+
+/**
+ * having a return statement in both the else if branches make the code slightly more clear
+ * and prevent further execution of the recursive calls when a match has been found
+ */
---
+++ src/com/raka/datastructures/array/searching/TernarySearchIterative.java
@@ -0,0 +1,57 @@
+package com.raka.datastructures.array.searching;
+
+import java.util.Scanner;
+
+public class TernarySearchIterative {
+
+    public static int searchElement(int[] array, int key){
+        int lb = 0;                                 // lower bound
+        int ub = array.length - 1;                  // upper bound
+
+        while(lb <= ub){
+            int mid1 = lb + (ub - lb)/3;            // 1st middle index
+            int mid2 = ub - (ub - lb)/3;            // 2nd middle index
+
+            if(array[mid1] == key){
+                return mid1;
+            }else if(array[mid2] == key){
+                return  mid2;
+            } else if (array[mid1] > key ) {           // sets the search space to 1st one-third part
+                ub = mid1 - 1;
+            } else if (array[mid2] < key) {            // sets the search space to 3rd one-third part
+                lb = mid2 + 1;
+            }else {                                    // sets the search space to 2nd one-third part
+                lb = mid1 + 1;
+                ub = mid2 - 1;
+            }
+        }
+
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int[] array = {12, 19, 22, 23, 25, 27, 33, 38, 42, 47, 50, 51, 59, 66};
+
+        System.out.println("Elements in the array - ");
+        for (int element : array){
+            System.out.print(element + "   ");
+        }
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("\nEnter the element to search - ");
+        int key = input.nextInt();                  // element to search
+
+        int index = searchElement(array, key);
+        if(index == -1){
+            System.out.println("Element Not Found");
+        }
+        else {
+            System.out.println("Element Found At Index " + index);
+        }
+    }
+}
+
+/**
+ * Time complexity - O(log n base 3)
+ * Space complexity - O(1)
+ */
