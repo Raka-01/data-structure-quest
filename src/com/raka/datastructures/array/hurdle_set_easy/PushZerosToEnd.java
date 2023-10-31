0f2e163fa41b71223696f194ea2081a5a5caeff8
Rakesh Kumar <rkbokaro00@gmail.com>
2023-10-31 13:28:40 +0530

    added-arraylist-push-zeros-to-end

--- src/com/raka/datastructures/array/hurdle_set_easy/PushZerosToEnd.java
+++ src/com/raka/datastructures/array/hurdle_set_easy/PushZerosToEnd.java
@@ -1,21 +1,21 @@
 package com.raka.datastructures.array.hurdle_set_easy;
 
 /**
- * Move all zeroes to end of array
- *
+ * Move all zeroes to end of array without affecting the order of non-zero elements
+ * <p>
  * Time complexity = O(n)
  * Auxiliary space = O(1)
  */
 public class PushZerosToEnd {
 
-    public static void pushZerosToEnd(int[] array){
+    public static void pushZerosToEnd(int[] array) {
         int length = array.length;
         int count = 0;
 
         // iterating through each element of the array
-        for (int i = 0; i < length; i++){
+        for (int i = 0; i < length; i++) {
             // for every non-zero elements in the array copy that element at index count of the array and count is incremented
-            if(array[i] != 0){
+            if (array[i] != 0) {
                 array[count] = array[i];
                 count++;
             }
@@ -25,15 +25,15 @@ public class PushZerosToEnd {
         since all non-zero elements are copied to front of array till index count-1,
         so now elements from index count to length-1 will be 0
          */
-        while (count < length){
+        while (count < length) {
             array[count] = 0;
             count++;
         }
     }
 
     // display elements of the array to the console
-    public static void displayArray(int[] array){
-        for (int element : array){
+    public static void displayArray(int[] array) {
+        for (int element : array) {
             System.out.print(element + "   ");
         }
         System.out.println();
--- src/com/raka/datastructures/array/hurdle_set_easy/PushZerosToEnd2.java
+++ src/com/raka/datastructures/array/hurdle_set_easy/PushZerosToEnd2.java
@@ -1,24 +1,29 @@
 package com.raka.datastructures.array.hurdle_set_easy;
 
 /**
- * Move all zeroes to end of array
- *Approach - use 0 as pivot element and whenever we see a non-zero element
+ * Move all zeroes to end of array without affecting the order of non-zero elements
+ * <p>
+ * Approach - use 0 as pivot element and whenever we see a non-zero element
  * we will swap it with the pivot element.
  * So all the non-zero element will come at the beginning
- *
+ * <p>
  * Time complexity = O(n)
  * Auxiliary space = O(1)
  */
 public class PushZerosToEnd2 {
 
     // pushes all zeros in the array to the end of the array
-    public static void pushZerosToEnd(int[] array){
+    public static void pushZerosToEnd(int[] array) {
         int length = array.length;
 
         int j = 0;
-        for(int i = 0; i < length; i++){
-            // first non-zero element will be swapped first, so order will not be lost
-            if(array[i] != 0){
+        for (int i = 0; i < length; i++) {
+            /*
+            first non-zero element will be swapped first,
+            so element will swap with either itself or zero,
+            hence, order will not be lost
+             */
+            if (array[i] != 0) {
                 // swap arr[i] and arr[j] & increment j
                 int temp = array[i];
                 array[i] = array[j];
@@ -29,8 +34,8 @@ public class PushZerosToEnd2 {
     }
 
     // display elements of the array to the console
-    public static void displayArray(int[] array){
-        for (int element : array){
+    public static void displayArray(int[] array) {
+        for (int element : array) {
             System.out.print(element + "   ");
         }
         System.out.println();
