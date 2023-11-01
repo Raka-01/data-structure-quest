f392ca2464a6a5a0248961e862b043e3ee16dd8f
Rakesh Kumar <rkbokaro00@gmail.com>
2023-11-01 16:18:36 +0530

    added-rearrange-array-max-min-form

--- src/com/raka/datastructures/array/hurdle_set_easy/RearrangeArrayMaxMin.java
+++ src/com/raka/datastructures/array/hurdle_set_easy/RearrangeArrayMaxMin.java
@@ -7,13 +7,13 @@ import java.util.Arrays;
  * (rearrange the array alternately so that first element should be a maximum value,
  * at second position minimum value, at third position second max,
  * at fourth position second min, and so on)
- *
+ * <p>
  * Approach -
  * - use an auxiliary array
  * - maintain two pointers one to the leftmost(smallest) element and the other to the rightmost(largest) element
  * - move both pointers toward each other and alternatively copy elements at these pointers to the auxiliary array
  * - copy the auxiliary array back to the original array
- *
+ * <p>
  * Time Complexity: O(n)
  * Auxiliary Space: O(n)
  */
@@ -21,28 +21,25 @@ import java.util.Arrays;
 public class RearrangeArrayMaxMin {
 
     // rearrange an array in maximum minimum form
-    public static void rearrangeMaxMin(int[] array){
+    public static void rearrangeMaxMin(int[] array) {
         Arrays.sort(array);             // uses dual-pivot quicksort algorithm - O(nlogn)
 
         int length = array.length;
         int[] temp = new int[length];
-        int p = 0, q = length-1;        // p & q are the two pointers pointing start and end index of array resp.
+        int p = 0, q = length - 1;        // p & q are the two pointers pointing start and end index of array resp.
 
-        for(int i = 0; i < length; i++){
-            if(i%2 == 0){                   // all the even positions (0, 2, 4, ...)
+        for (int i = 0; i < length; i++) {
+            if (i % 2 == 0) {                   // all the even positions (0, 2, 4, ...)
                 temp[i] = array[q];         // stores larger elements
                 q--;
-            }
-            else {                          // all the odd positions (1,3,5, ...)
+            } else {                          // all the odd positions (1,3,5, ...)
                 temp[i] = array[p];         // stores the smaller elements
                 p++;
             }
         }
 
         // copy all the elements back to the original array
-        for (int i = 0; i < length; i++){
-            array[i] = temp[i];
-        }
+        System.arraycopy(temp, 0, array, 0, length);
     }
 
     // display the elements of the array to the console
