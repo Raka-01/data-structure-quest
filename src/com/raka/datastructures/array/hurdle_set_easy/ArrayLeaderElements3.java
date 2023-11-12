0aee603efbe6769a9e0ba7dff1adb18225d00f42
Rakesh Kumar <rkbokaro00@gmail.com>
2023-11-13 00:06:49 +0530

    updated-array-leader-elements

--- src/com/raka/datastructures/array/hurdle_set_easy/ArrayLeaderElements3.java
+++ src/com/raka/datastructures/array/hurdle_set_easy/ArrayLeaderElements3.java
@@ -16,7 +16,7 @@ import java.util.Stack;
  * - finally, pop out element one by one
  * <p>
  * Time complexity - O(n)
- * Auxiliary space - O(1)
+ * Auxiliary space - O(n)
  */
 
 public class ArrayLeaderElements3 {
@@ -32,11 +32,12 @@ public class ArrayLeaderElements3 {
             // if current element is the max element till current iteration, it is a leader element
             if (array[i] > maxElement){
                 maxElement = array[i];          // max element is updated when a new one is found
-                stack.push(array[i]);
+                stack.push(array[i]);           // leader element is pushed into the stack
             }
         }
 
         System.out.println("Leader elements present in the array - ");
+        // all the leader elements are displayed to console in order they are in the array
         while (!stack.empty()){
             System.out.print(stack.pop() + "    ");
         }
