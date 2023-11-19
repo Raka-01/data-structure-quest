3245f2ae7723f6fb26370abf38919980099ff88d
Rakesh Kumar <rkbokaro00@gmail.com>
2023-11-19 23:26:39 +0530

    implemented-triangle-pattern

--- src/com/raka/ds/striver/basics/java/PatternTriangle.java
+++ src/com/raka/ds/striver/basics/java/PatternTriangle.java
@@ -5,19 +5,19 @@ import java.util.Scanner;
 /**
  * Striver 1.2
  * Print the following pattern :
- *      *  *  *  *  *
- *      *  *  *  *  *
- *      *  *  *  *  *
- *      *  *  *  *  *
+ *      *
+ *      *  *
+ *      *  *  *
+ *      *  *  *  *
  *      *  *  *  *  *
  */
 
-public class PatternSquare {
+public class PatternTriangle {
 
-    public static void patternSquare(int n){
-        System.out.println("Pattern -");
-        for (int  i = 0; i < n; i++){
-            for (int j = 0; j < n; j++){
+    public static void patternTriangle(int n){
+        System.out.println("Pattern - ");
+        for (int i = 0; i < n; i++){
+            for (int j = 0; j <= i; j++){
                 System.out.print("*  ");
             }
             System.out.println();
@@ -30,6 +30,6 @@ public class PatternSquare {
         System.out.println("Enter the value of n - ");
         int n = sc.nextInt();
 
-        patternSquare(n);
+        patternTriangle(n);
     }
 }
