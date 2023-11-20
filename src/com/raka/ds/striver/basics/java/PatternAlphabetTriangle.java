5d769f39476481d21965d0b15c1ec0c61ee32eee
Rakesh Kumar <rkbokaro00@gmail.com>
2023-11-20 23:49:23 +0530

    implemented-alphabet-triangle-pattern

---
+++ src/com/raka/ds/striver/basics/java/PatternAlphabetTriangle.java
@@ -0,0 +1,37 @@
+package com.raka.ds.striver.basics.java;
+
+import java.util.Scanner;
+
+/**
+ * Striver 1.2
+ * Print the following pattern :
+ *      A
+ *      AB
+ *      ABC
+ *      ABCD
+ *      ABCDE
+ */
+
+public class PatternAlphabetTriangle {
+
+    public static void patternAlphabetTriangle(int n) {
+        System.out.println("Pattern - ");
+        for (int i = 0; i < n; i++) {
+            int k = 65;
+            for (int j = 0; j <= i; j++) {
+                System.out.print((char) k);
+                k++;
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the value of n - ");
+        int n = sc.nextInt();
+
+        patternAlphabetTriangle(n);
+    }
+}
