afdf45bcebe4d0bbae5dc5bdcec3d3a42768761b
Rakesh Kumar <rkbokaro00@gmail.com>
2023-11-27 23:34:24 +0530

    implemented-gcd-of-two-numbers

---
+++ src/com/raka/ds/striver/basics/java/GCDofNumber.java
@@ -0,0 +1,39 @@
+package com.raka.ds.striver.basics.java;
+
+import java.util.Scanner;
+
+/**
+ * Striver 1.4
+ * Calculate the GCD of two given numbers
+ */
+public class GCDofNumber {
+
+    public static int calculateGCD(int num1, int num2) {
+        int gcd = 1;
+
+        /*
+        starts with 2 as 1 is always a divisor
+         */
+        for (int i = 2; i <= Math.min(num1, num2); i++) {
+            if (num1 % i == 0 && num2 % i == 0) {
+                gcd = i;
+            }
+        }
+
+        return gcd;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int num1, num2;
+        System.out.println("Enter number1 = ");
+        num1 = sc.nextInt();
+
+        System.out.println("Enter number2 = ");
+        num2 = sc.nextInt();
+
+        int gcd = calculateGCD(num1, num2);
+        System.out.println("GCD of the given 2 numbers  - " + gcd);
+    }
+}
--- src/com/raka/ds/striver/basics/java/ReverseNumber.java
+++ src/com/raka/ds/striver/basics/java/ReverseNumber.java
@@ -3,6 +3,7 @@ package com.raka.ds.striver.basics.java;
 import java.util.Scanner;
 
 /**
+ * Striver 1.4
  * There is a song concert going to happen in the city.
  * The price of each ticket is equal to the number obtained
  * after reversing the bits of a given 32 bits unsigned integer 'n'.
